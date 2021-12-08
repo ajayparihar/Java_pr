dded2ecbe54c7e74d4cefa06c51ab4ca5e57615a
Bheb Developer <bhebdeveloper@gmail.com>
2021-12-08 19:32:18 +0530

    Full Commit

--- src/com/company/week.java
+++ src/com/company/week.java
@@ -9,32 +9,18 @@ public class week {
 		Scanner scan= new Scanner(System.in);
 		int d= scan.nextInt();
 		int day=Math.abs(d);
-		switch(day) {
-		case 1:
-			System.out.print("MONDAY");
-			break;
-		case 2:
-			System.out.print("TUESDAY");
-			break;
-		case 3:
-			System.out.print("WEDNESDAY");
-			break;
-		case 4:
-			System.out.print("THURSDAY");
-			break;
-		case 5:
-			System.out.print("FRIDAY");
-			break;
-		case 6:
-			System.out.print("SATURDAY");
-			break;
-		case 7:
-			System.out.print("SUNDAY");
-			break;
-			
-			default:
-			System.out.print("Invalid input");
-			scan.close();
+		switch (day) {
+			case 1 -> System.out.print("MONDAY");
+			case 2 -> System.out.print("TUESDAY");
+			case 3 -> System.out.print("WEDNESDAY");
+			case 4 -> System.out.print("THURSDAY");
+			case 5 -> System.out.print("FRIDAY");
+			case 6 -> System.out.print("SATURDAY");
+			case 7 -> System.out.print("SUNDAY");
+			default -> {
+				System.out.print("Invalid input");
+				scan.close();
+			}
 		}
 	}
 }
